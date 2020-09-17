c93dff5bab58f57205050fb192922a72366fa1d5
rajvichaudhari07 <69470226+rajvichaudhari07@users.noreply.github.com>
2020-09-17 23:47:46 +0530

    Create primeno.c

---
+++ primeno.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int isPrime(int, int);
+int main()
+{
+    printf("\n\n\t\tStudytonight - Best place to learn\n\n\n");
+    int num, prime;
+    printf("Enter a positive number to check if Prime: ");
+    scanf("%d", &num);
+    prime = isPrime(num, num/2);
+    if(prime == 1)
+    {
+        printf("\n\n%d is a prime number\n\n", num);
+    }
+    else
+    {
+        printf("\n\n%d is a Composite number\n\n", num);
+    }
+    printf("\n\n\t\t\tCoding is Fun !\n\n\n");
+    return 0;
+}
+int isPrime(int n, int i)
+{
+    if(i == 1)
+        return 1;   
+    else
+    {
+        if(n%i == 0)
+            return 0;
+        else
+            isPrime(n, i-1);    
+    }
+}
